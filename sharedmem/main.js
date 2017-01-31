e9f569608087768c698a3c9bf23cc2d669f60f53
Steffen Peleikis <steffen.peleikis@hytera.de>
2017-01-31 21:27:16 +0100

    added mandelbrot examples

--- sharedmem/main.js
+++ sharedmem/main.js
@@ -10,7 +10,35 @@
   }
 
 
+  // mandelbrot-fractal.js
 
   const worker = new Worker('worker.js');
+
+  // To be shared
+  const sharedBuffer = new SharedArrayBuffer( // (A)
+    10 * Int32Array.BYTES_PER_ELEMENT); // 10 elements
+
+
+  // Local only
+  const sharedArray = new Int32Array(sharedBuffer); // (B)
+
+
+  Lock.initialize(sharedArray, 0);
+  const lock = new Lock(sharedArray, 0);
+
+  lock.lock();
+  console.log('locked');
+  worker.postMessage({sharedBuffer}); // clone
+
+  setTimeout(function () {
+    lock.unlock();
+    console.log('unlocked');
+    lock.lock();
+    console.log('got lock');
+    //lock.unlock();
+
+
+  }, 1500);
+
 
 })(); // END iife
--- sharedmem/worker.js
+++ sharedmem/worker.js
@@ -2,6 +2,22 @@
  * Created by peleikis on 31.01.2017.
  */
 "use strict";
+importScripts('lock_es6.js');
+
 (function () { // BEGIN iife
-  document.getElementById('output').textContent = 'I am a worker!';
+
+  self.addEventListener('message', function (event) {
+    const {sharedBuffer} = event.data;
+    const sharedArray = new Int32Array(sharedBuffer); // (A)
+    const lock = new Lock(sharedArray, 0);
+    // ···
+
+    console.log('\tlocking');
+    lock.lock();
+    console.log('\tgot lock');
+    lock.unlock();
+
+  });
+
+
 })(); // END iife
